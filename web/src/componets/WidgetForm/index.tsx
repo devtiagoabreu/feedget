9d678ec983a448c8ef9b60cf352086ae1cfa9571
Tiago de Abreu <devtiagoabreu@gmail.com>
2022-05-07 20:51:33 -0300

    component communication

--- web/src/componets/WidgetForm/index.tsx
+++ web/src/componets/WidgetForm/index.tsx
@@ -6,6 +6,7 @@ import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
     BUG:{
@@ -36,22 +37,25 @@ export type FeedbackType = keyof typeof feedbackTypes;
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu Feedback</span>
-
-                <CloseButton />
-            </header>
-
+            
             {!feedbackType ? ( 
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>Hello Marte</p>
+                <FeedbackContentStep 
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ por <a className="underline underline-offset-2" href="http://atriostech.com.br/tiago">Tiago de Abreu</a> 
             </footer>
+
         </div>
 
     );
