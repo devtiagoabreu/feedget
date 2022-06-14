258d4ddb3a8b8eae39d39f725dfa2cc6518c46e5
Tiago de Abreu <devtiagoabreu@gmail.com>
2022-06-14 19:09:34 -0300

    prisma start

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,14 @@
-const teste = 1;
+import express from 'express'
+
+const app = express();
+
+app.get('/users', (req, res) => {
+    return res.send('Hello World')
+
+})
+
+
+
+app.listen(3333, () => {
+    console.log('HTTP server running!')
+});
