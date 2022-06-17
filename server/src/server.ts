f2514b7ddf3b917464d7a8e92bab468665df989b
Tiago de Abreu <devtiagoabreu@gmail.com>
2022-06-17 11:11:32 -0300

    rota post create feedbacks

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,23 @@
 import express from 'express'
+import { prisma } from './prisma';
+
 
 const app = express();
 
-app.get('/users', (req, res) => {
-    return res.send('Hello World')
+app.use(express.json());
 
+app.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body;
+
+    const feedback = await prisma.feedback.create({
+        data: {
+            type,
+            comment,
+            screenshot,
+        }
+    })
+
+    return res.status(201).json({ data: feedback });
 })
 
 
